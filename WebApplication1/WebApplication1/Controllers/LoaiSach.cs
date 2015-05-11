66fbbb552cdd586b85c306236c082a9b22ba4d74
dungtt <dungthantai@gmail.com>
2015-05-11 09:23:37 +0700

    1.0

---
+++ WebApplication1/WebApplication1/Controllers/LoaiSach.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class LoaiSach
+    {
+        public string MaLoai { get; set; }
+        public string Tenloai { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/NhaXB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class NhaXB
+    {
+        public string MaNXB { get; set; }
+        public string TenNXB { get; set; }
+        public string Diachi { get; set; }
+        public string SDT { get; set; }
+        public string Email { get; set; }
+    }
+}

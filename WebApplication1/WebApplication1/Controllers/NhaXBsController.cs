e7b6ac8a021e16bd505f921d8c530f10552337f5
dungtt <dungthantai@gmail.com>
2015-05-15 09:14:48 +0700

    Update Ass1

---
+++ WebApplication1/WebApplication1/Controllers/NhaXBsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.DAL;
+
+namespace WebApplication1.Controllers
+{
+    public class NhaXBsController : Controller
+    {
+        private SachContext db = new SachContext();
+
+        // GET: NhaXBs
+        public ActionResult Index()
+        {
+            return View(db.NhaXBs.ToList());
+        }
+
+        // GET: NhaXBs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NhaXB nhaXB = db.NhaXBs.Find(id);
+            if (nhaXB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhaXB);
+        }
+
+        // GET: NhaXBs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NhaXBs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaNXB,TenNXB,Diachi,SDT,Email")] NhaXB nhaXB)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NhaXBs.Add(nhaXB);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(nhaXB);
+        }
+
+        // GET: NhaXBs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NhaXB nhaXB = db.NhaXBs.Find(id);
+            if (nhaXB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhaXB);
+        }
+
+        // POST: NhaXBs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaNXB,TenNXB,Diachi,SDT,Email")] NhaXB nhaXB)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nhaXB).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nhaXB);
+        }
+
+        // GET: NhaXBs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NhaXB nhaXB = db.NhaXBs.Find(id);
+            if (nhaXB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhaXB);
+        }
+
+        // POST: NhaXBs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            NhaXB nhaXB = db.NhaXBs.Find(id);
+            db.NhaXBs.Remove(nhaXB);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/TacgiasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.DAL;
+
+namespace WebApplication1.Controllers
+{
+    public class TacgiasController : Controller
+    {
+        private SachContext db = new SachContext();
+
+        // GET: Tacgias
+        public ActionResult Index()
+        {
+            return View(db.Tacgias.ToList());
+        }
+
+        // GET: Tacgias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tacgia tacgia = db.Tacgias.Find(id);
+            if (tacgia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tacgia);
+        }
+
+        // GET: Tacgias/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tacgias/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaTG,Tentacgia,Diachi,SDT,Email")] Tacgia tacgia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tacgias.Add(tacgia);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tacgia);
+        }
+
+        // GET: Tacgias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tacgia tacgia = db.Tacgias.Find(id);
+            if (tacgia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tacgia);
+        }
+
+        // POST: Tacgias/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaTG,Tentacgia,Diachi,SDT,Email")] Tacgia tacgia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tacgia).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tacgia);
+        }
+
+        // GET: Tacgias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tacgia tacgia = db.Tacgias.Find(id);
+            if (tacgia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tacgia);
+        }
+
+        // POST: Tacgias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tacgia tacgia = db.Tacgias.Find(id);
+            db.Tacgias.Remove(tacgia);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
